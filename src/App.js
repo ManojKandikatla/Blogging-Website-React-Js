8b6c16ca97a78135745b10ca42681ef2e6b42184
ManojKandikatla <kandikatlamanojkumar58681@gmailcom>
2025-07-15 19:37:47 +0530

    Deleted comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Registration from "./pages/Registration/Registration";
@@ -28,48 +27,3 @@ function App() {
 
 export default App;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //import logo from './logo.svg';
-// import "./App.css";
-// import Login from "./pages/Login/Login";
-// import Registration from "./pages/Registration/Registration";
-// import Home from "./pages/Home/Home";
-// import Navbar from "./Components/Navbar/Navbar";
-// import Footer from "./Components/Footer/Footer";
-// import CategoryBlogs from "./pages/CategoryBlogs/CategoryBlogs";
-// import DedicatedBlog from "./pages/DedicatedBlogs/DedicatedBlog";
-// import { Routes, Route } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/home" element={<Home />} />
-//         <Route path="/" element={<Home />} />
-//         <Route path="/Login" element={<Login />} />
-//         <Route path="/Registration" element={<Registration />} />
-//         <Route path="/blogs/category/:category" element={<CategoryBlogs />} />
-//         <Route path="/blog/:id" element={<DedicatedBlog />} />
-//         <Route path="*" element={<h2>Welcome...</h2>} />
-//       </Routes>
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
